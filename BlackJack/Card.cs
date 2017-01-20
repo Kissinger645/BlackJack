7e1949572691cf38a040ef8896331248b90f6617
Bobby Kissinger <kissinger645@gmail.com>
2017-01-19 22:19:29 -0500

    added notes

---
+++ BlackJack/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Card
+    {
+        string cardNumber;
+        int cardValue;
+        string cardSuit;
+
+    }
+}
