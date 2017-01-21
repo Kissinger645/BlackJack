c766c2d65c5f426f2a0c352443f7fb80309d9524
Bobby Kissinger <kissinger645@gmail.com>
2017-01-21 16:47:35 -0500

    just notes for later

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -3,16 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*
-Deal first card to player
-second card to dealer
-third to player
-fourth to dealer
-check for blackjack
-ask player to hit or stand
-Dealer = if cardValue < 16, must hit until cardValue >= 17
-when both states are stand, determine winner
-*/
+
 namespace BlackJack
 {
     class Program
@@ -34,8 +25,12 @@ namespace BlackJack
             Console.WriteLine("Dealer has:");
             dealerHand.ForEach(Console.WriteLine);
             //check for blackjack
+            //ask player to hit or stand
+            //if cardValue > 21, player busts
+            //Dealer = if cardValue < 16, must hit until cardValue >= 17
+            //when both states are stand, determine winner
 
-            
+
 
             Console.ReadKey();
         }
